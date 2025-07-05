9cf27767104c1fbfbe46c764ea601b0055d4d6e8
AsurNikul <nikulsolanki707@gmail.com>
2025-07-06 02:58:58 +0530

    app renamed

--- ios/GlobexSpintexTests/GlobexSpintexTests.m
+++ ios/GlobexSpintexTests/GlobexSpintexTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GlobalSpintexTests : XCTestCase
+@interface GlobexSpintexTests : XCTestCase
 
 @end
 
-@implementation GlobalSpintexTests
+@implementation GlobexSpintexTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
